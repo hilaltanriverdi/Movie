e2ee4da647d52d58344cb47f0d6ee21c7c8cb17a
tnrvrd <tanriverdi.hilal@gmail.com>
2016-03-13 20:01:59 +0200

    .

--- src/main/java/com/infoowl/controller/FilmController.java
+++ src/main/java/com/infoowl/controller/FilmController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 /**
  * Created by hilal on 3/10/2016.
  */
@@ -17,59 +19,70 @@ public class FilmController {
     @Autowired
    private FilmRepository filmRepository;
 
-
-  /*  @RequestMapping("/")
+    /**
+     *save a film.
+     *@return process message
+     */
+    @RequestMapping("/create")
     @ResponseBody
     public String create() {
         Film film=new Film();
         film.setFilmName("the it crowd");
+        film.setFilmType("comedi");
         filmRepository.save(film);
         System.out.println("eklendi");
-        return "its added";
-    }
-    */
-
-    @RequestMapping("/create")
-    @ResponseBody
-    public String create(String filmAdı, String filmName) {
-        Film film = null;
-        try {
-            film = new Film(filmAdı, filmName);
-            filmRepository.save(film);
-        }
-        catch (Exception ex) {
-           return "Error creating the film: " + ex.toString();
-        }
-        return "Film succesfully created! (id = " + film.getFilmId() + ")";
+        return "successfully added";
     }
 
 
+    /**
+     * delete the film
+     * @return process message
+     */
     @RequestMapping("/delete")
     @ResponseBody
-    public String delete(long id) {
-        try {
-            Film film = new Film(id);
-            filmRepository.delete(film);
-        }
-        catch (Exception ex) {
-            return "Error deleting the film:" + ex.toString();
-        }
-        return "Film succesfully deleted!";
-   }
+    public String delete(){
+        Film film=new Film(1);
+        filmRepository.delete(film);
+        return "successfully deleted";
+    }
 
+    /**
+     * update the film
+     * @return process message
+     */
     @RequestMapping("/update")
     @ResponseBody
-    public String updateUser(long id,String filmName, String filmType) {
-        try {
-            Film film = filmRepository.findOne(id);
-            film.setFilmName(filmName);
-            film.setFilmType(filmType);
-            filmRepository.save(film);
-        }
-        catch (Exception ex) {
-            return "Error updating the film: " + ex.toString();
-        }
-       return "Film succesfully updated!";
+    public String update(){
+        long id=3;
+        Film film=filmRepository.findOne(id);
+        film.setFilmName("new Name");
+        film.setFilmType("new Type");
+        filmRepository.save(film);
+        return "successfully updated";
     }
+
+    /**
+     * list all films
+     * @return film field
+     */
+    @RequestMapping("/list")
+    @ResponseBody
+    public String list(){
+        Iterable<Film> filmList=filmRepository.findAll();
+        StringBuilder builder=new StringBuilder();
+        for (Film film : filmList) {
+            builder.append(film.getFilmId()+" ")
+                    .append(film.getFilmName()+" ")
+                    .append(film.getFilmType()+"/n");
+
+        }
+        return builder.toString();
+
+    }
+
+
+
+
 
 }
